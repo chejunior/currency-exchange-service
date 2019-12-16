caae69b3c663ac92a3ece75092c980aa39b0909b
Maichel Castro <mike.castro.b@gmail.com>
2019-12-15 23:05:37 -0500

    Finishing tutorial

--- src/main/java/com/maik/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/maik/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,9 +1,7 @@
 package com.maik.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
-import javax.websocket.server.PathParam;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private Environment environment;
@@ -23,6 +23,7 @@ public class CurrencyExchangeController {
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue value = repository.findByFromAndTo(from, to);
 		value.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}", value);
 		return value;
 	}
 
--- src/main/java/com/maik/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/maik/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,12 +2,20 @@ package com.maik.microservices.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+	
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
